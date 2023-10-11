b0dce9892085c814794baa05dbd3cf17422312a8
Aditya-078 <200303105146@paruluniversity.ac.in>
2023-10-11 18:03:15 +0530

    Register with backend
    -Dependencies - axios, toastify
    
    -Learned how to use axios & toastify.
    
    -Making register part work with backend system.
    
    -Added Logic for authSlice and making it work with redux.
    
    -Regsiter form from frontend will send data to backend and store it as cookie session.

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000",
+    "https://infispec-app.vercel.app" 
+    ],
+    credentials: true
+})
+);
 
 app.use('/uploads', express.static(path.join(__dirname, "uploads")))
 
--- frontend/src/pages/auth/Register.js
+++ frontend/src/pages/auth/Register.js
@@ -1,10 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../../components/card/Card'
 import styles from './auth.module.scss'
 import { TiUserAddOutline } from 'react-icons/ti'
 import { Link } from 'react-router-dom'
+import {toast} from "react-toastify"
+import { registerUser, ValidateEmail } from '../../services/authService'
+import { SET_LOGIN, SET_NAME } from "../../redux/features/auth/authSlice";
+import { useDispatch } from "react-redux";
+import {  useNavigate } from "react-router-dom";
+
+
+const  initialState= {
+  name:"",
+  email:"",
+  password:"",
+  password2:"",
+}
 
 const Register = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setformData] = useState(initialState);
+  const { name, email, password, password2 } = formData;
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setformData({ ...formData, [name]: value });
+  };
+
+  const register = async (e) => {
+    e.preventDefault();
+
+    if (!name || !email || !password) {
+      return toast.error("All fields are required");
+    }
+    if (password.length < 6) {
+      return toast.error("Passwords must be up to 6 characters");
+    }
+    if (!ValidateEmail(email)) {
+      return toast.error("Please enter a valid email");
+    }
+    if (password !== password2) {
+      return toast.error("Passwords do not match");
+    }
+
+    const userData = {
+      name,
+      email,
+      password,
+    };
+    setIsLoading(true);
+    try {
+      const data = await registerUser (userData);
+       console.log(data);
+      
+      
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error.message)
+    }
+  };
+
+
   return (
     <div className={`container ${styles.auth}`}>
       
@@ -15,13 +74,14 @@ const Register = () => {
           </div>
           <h2>Register</h2>
 
-          <form>
+          <form onSubmit={register}>
             <input
               type="text"
               placeholder="Name"
               required
               name="name"
-              
+              value={name}
+              onChange={handleInputChange}
               
             />
             <input
@@ -29,22 +89,24 @@ const Register = () => {
               placeholder="Email"
               required
               name="email"
-              
-              
+              value={email}
+              onChange={handleInputChange}
             />
             <input
               type="password"
               placeholder="Password"
               required
               name="password"
-              
+              value={password}
+              onChange={handleInputChange}
             />
             <input
               type="password"
               placeholder="Confirm Password"
               required
               name="password2"
-              
+              value={password2}
+              onChange={handleInputChange}
             />
             <button type="submit" className="--btn --btn-primary --btn-block">
               Register
